4283d4a857142edbb17de4adc0180cb5d89b7e90
shaamilshan <shmlvk123@gmail.com>
2024-07-22 14:43:53 +0530

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,7 +13,7 @@ function Header() {
 
   return (
     <div className='Header'>
-      <img src={eDex} alt="" className='edex-logo' />
+      <img src={eDex} alt="eDex Logo" className='edex-logo' />
       <div className={`menu-icon ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
         <div className="bar"></div>
         <div className="bar"></div>
@@ -23,8 +23,8 @@ function Header() {
         <NavLink to='/home' activeClassName="active"><li className='header-li'>Home</li></NavLink>
         <NavLink to='/about' activeClassName="active"><li className='header-li'>About</li></NavLink>
         <NavLink to='/blog' activeClassName="active"><li className='header-li'>Blog</li></NavLink>
+        <NavLink to='/contact' activeClassName="active"><li className='header-li'>Contact</li></NavLink>
       </ul>
-      <button onClick={() => navigate('/contact')} className='header-btn'><p>Contact</p></button>
     </div>
   );
 }
